ad3f09388d0b7a75f6e48d5572e20b85c40e0328
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-07-20 02:18:35 -0500

    fixes and new summary feature

--- api_summary.go
+++ api_summary.go
@@ -1,7 +1,20 @@
 package neocortex
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func registerSummaryAPI(r *gin.RouterGroup) {
-	
+	"github.com/gin-gonic/gin"
+)
+
+func (api *API) registerSummaryAPI(r *gin.RouterGroup) {
+	r.GET("/summary", func(c *gin.Context) {
+		summary, err := api.repository.Summary()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"data": summary,
+		})
+	})
 }
