0cbddac78827f972090cdb239942d929a7c6596b
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-09-26 13:15:18 -0500

    simple fix

--- cortex_onmessage.go
+++ cortex_onmessage.go
@@ -2,6 +2,7 @@ package neocortex
 
 import (
 	"log"
+	"strings"
 	"time"
 
 	"github.com/k0kubun/pp"
@@ -28,6 +29,9 @@ func (engine *Engine) onMessage(channel CommunicationChannel, c *Context, in *In
 		dialog.Ins = append(dialog.Ins, &InputRecord{At: time.Now(), Input: *in})
 	}
 
+	if in.Data.Type == InputText {
+		in.Data.Value = strings.ReplaceAll(in.Data.Value, "\n", "")
+	}
 	pp.Println(in)
 
 	out, err := engine.cognitive.GetProtoResponse(c, in)
